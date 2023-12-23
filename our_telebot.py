7526c658642496fffcdfd3b340f0e599c7490518
Alex <kalinakrasnaya1990@gmail.com>
2023-12-23 19:50:13 +0700

    some bugs fixed

--- our_telebot.py
+++ our_telebot.py
@@ -1,223 +1,96 @@
-from random import random
-
 import telebot
 from telebot import TeleBot, types
 
 
+menus = {"User" : ["Sign up", "Sign in"],
+        "Init": ["World", "Stats"],
+        "World": ["Move", "Characters"],
+        }
+
 class GameBotHandler():
 
     @staticmethod
-    def create_cancel_button():
-        """
-        Create a cancel button for a ReplyKeyboardMarkup.
-
-        Returns:
-            types.ReplyKeyboardMarkup: The created cancel button markup.
-        """
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        markup.add('❌ Отмена')
-        return markup
+    def create_in_up_buttons(bot, message):
+        markup = types.ReplyKeyboardMarkup()
+        markup.add(types.KeyboardButton(text = "Sign up"),
+                   types.KeyboardButton(text = "Sign in"))
+        bot.send_message(message.chat.id, "Welcome", reply_markup=markup)
 
     @staticmethod
-    def create_in_up_buttons():
-        """
-        Creates a ReplyKeyboardMarkup with three buttons: "🛃 Зарегистрироваться", "✅ Войти", and "❌ Отмена".
-
-        Returns:
-            types.ReplyKeyboardMarkup: The created ReplyKeyboardMarkup object.
-        """
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        markup.add(types.KeyboardButton(text = "🛃 Зарегистрироваться"),
-                   types.KeyboardButton(text = "✅ Войти"),
-                   types.KeyboardButton(text = "❌ Отмена"))
-        return markup
+    def create_user_in_db(bot, message):
+        markup = types.ReplyKeyboardMarkup()
+        markup.add(types.KeyboardButton(text = "Sign up"),
+                   types.KeyboardButton(text = "Sign in"))
+        bot.send_message(message.chat.id, "Welcome", reply_markup=markup)
 
 
-    @staticmethod
-    def create_game_buttons():
-        """
-        Creates game buttons for the user interface.
-
-        Returns:
-            types.ReplyKeyboardMarkup: The markup containing the game buttons.
-        """
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        markup.row(types.KeyboardButton(text = "👩‍🎨 Персонажи"),
-                   types.KeyboardButton(text = "🧛‍♂️ Враги"))
-        markup.row(types.KeyboardButton(text = "🎲 Правила"),
-                   types.KeyboardButton(text = "🌍 Карта"),)
-        markup.row(types.KeyboardButton(text = "🥷 Герой"),
-                   types.KeyboardButton(text = "❌ Выход"))
-        return markup
-
-    @staticmethod
-    def create_dict_buttons(npc_dict):
-        """
-        Generate a ReplyKeyboardMarkup with buttons based on the given npc_dict.
-
-        Args:
-            npc_dict (dict): A dictionary containing the NPC data.
-
-        Returns:
-            types.ReplyKeyboardMarkup: The generated ReplyKeyboardMarkup object.
-        """
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        for word in npc_dict:
-            value = list(word.values())[0]
-            markup.add(types.KeyboardButton(text = f'👳 {value}'))
-        markup.row(types.KeyboardButton(text = "❌ Назад"))
-        return markup
-
-    @staticmethod
-    def create_enemy_list_buttons(npc_dict):
-        """
-        Generate a ReplyKeyboardMarkup with buttons based on the given npc_dict.
-
-        Args:
-            npc_dict (dict): A dictionary containing the NPC data.
-
-        Returns:
-            types.ReplyKeyboardMarkup: The generated ReplyKeyboardMarkup object.
-        """
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        for word in npc_dict:
-            value = list(word.values())[0]
-            markup.add(types.KeyboardButton(text = f'🧛‍♂️ {value}'))
-        markup.row(types.KeyboardButton(text = "❌ Назад"))
-        return markup
 
 
-    @staticmethod
-    def create_location_buttons(locations):
-        """
-        Create location buttons for a given list of locations.
-
-        Args:
-            locations (list): A list of locations.
-
-        Returns:
-            types.ReplyKeyboardMarkup: The markup containing the location buttons.
-        """
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        for location in locations:
-            value = '🌏 ' + location['name']
-            markup.add(types.KeyboardButton(text = value))
-        markup.row(types.KeyboardButton(text = "❌ Назад"))
-        return markup
-
-    @staticmethod
-    def create_enemy_buttons():
-        """
-        Creates enemy buttons for the user interface.
-
-        Returns:
-            types.ReplyKeyboardMarkup: The created markup for the enemy buttons.
-        """
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        markup.row(types.KeyboardButton(text = "❓ Инфо"),
-                   types.KeyboardButton(text = "⚔️ Бой"),
-                   types.KeyboardButton(text = "❌ Назад"))
-        return markup
 
 
-    @staticmethod
-    def create_npcs_buttons():
-        """
-        Creates a ReplyKeyboardMarkup object with buttons for interacting with NPCs.
-
-        Returns:
-            types.ReplyKeyboardMarkup: The ReplyKeyboardMarkup object with the NPC buttons.
-        """
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        markup.row(types.KeyboardButton(text = "❓ Инфо"),
-                   types.KeyboardButton(text = "🧩 Взять квест"),
-                   types.KeyboardButton(text = "🗣 Поговорить"),
-                   types.KeyboardButton(text = "❌ Назад"))
-        return markup
 
 
-    @staticmethod
-    def create_location_act_buttons():
-        """
-        Creates a set of location action buttons for a chatbot.
 
-        Returns:
-            types.ReplyKeyboardMarkup: The created ReplyKeyboardMarkup object.
-        """
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        markup.row(types.KeyboardButton(text = "❓ Инфо"),
-                   types.KeyboardButton(text = "⏩ Перейти"),
-                   types.KeyboardButton(text = "❌ Назад"))
-        return markup
-
-    @staticmethod
-    def create_locations_buttons(locations):
-        """
-        Creates a ReplyKeyboardMarkup with buttons for each location.
-
-        Args:
-            locations (list): A list of dictionaries representing locations. Each dictionary should have a 'name' key.
-
-        Returns:
-            types.ReplyKeyboardMarkup: The created ReplyKeyboardMarkup object.
-        """
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        for word in locations:
-            markup.add(types.KeyboardButton(text = word['name']))
-        markup.row(types.KeyboardButton(text = "Back"))
-        return markup
-
-    @staticmethod
-    def create_hero_buttons():
-        """
-        Create and return a ReplyKeyboardMarkup object containing hero buttons.
-
-        This static method creates a ReplyKeyboardMarkup object with hero buttons
-        for the user interface. The buttons include "🧳 Инвентарь", "Инфо",
-        "Лечение", "Квесты", and "Назад". The resize_keyboard parameter
-        is set to True to allow the keyboard to be resized.
-
-        Returns:
-            markup (types.ReplyKeyboardMarkup): The created ReplyKeyboardMarkup object.
-        """
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        markup.row(
-                   types.KeyboardButton(text = "🧳 Инвентарь"),
-                   types.KeyboardButton(text = "🧩 Квесты"),
-                   types.KeyboardButton(text = "➕ Лечение"))
-        markup.row(
-                   types.KeyboardButton(text = "❓ Инфо"),
-                   types.KeyboardButton(text = "❌ Назад"))
-        return markup
-
-    @staticmethod
-    def create_quests_list_buttons(quests):
-        """
-        Create a list of buttons for the given quests.
-
-        Args:
-            quests (list): A list of quest objects.
-
-        Returns:
-            markup (types.ReplyKeyboardMarkup): The keyboard markup with the buttons.
-        """
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        for quest in quests:
-            markup.add(types.KeyboardButton(text = quest.get_name()))
-        markup.row(types.KeyboardButton(text = "❌ Назад"))
-        return markup
+    # def CreateNpcButtons(self, message):
+    #     markup = types.ReplyKeyboardMarkup(row_width=3, resize_keyboard=True)
+    #     chars = types.KeyboardButton("cButton 1")
+    #     items = types.KeyboardButton("cButton 2")
+    #     back_bttn = types.KeyboardButton("cBack")
+    #     markup.add(chars, items, back_bttn)
+    #     self.send_message(message.chat.id, "this is the wrapper to characters",
+    #                     reply_markup=markup)
 
 
-    @staticmethod
-    def create_answer_buttons(count):
-        """
-        Creates a reply keyboard markup with the specified number of answer buttons.
+    # def CreateItemsButtons(self, message):
+    #     markup = types.ReplyKeyboardMarkup(row_width=3, resize_keyboard=True)
+    #     chars = types.KeyboardButton("iButton 1")
+    #     items = types.KeyboardButton("iButton 2")
+    #     back_bttn = types.KeyboardButton("iBack")
+    #     markup.add(chars, items, back_bttn)
+    #     self.send_message(message.chat.id, "this is the wrapper to items",
+    #                       reply_markup=markup)
+#********************************************************************************
 
-        :param count: An integer representing the number of answer buttons to create.
-        :return: A ReplyKeyboardMarkup object containing the reply keyboard markup.
-        """
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        for i in range(count):
-            markup.add(types.KeyboardButton(text = i+1))
-        markup.row(types.KeyboardButton(text = "❌ Окончить беседу"))
-        return markup
+
+        # start callbacks
+        # @.message_handler(commands=['start'])
+        # def handle_init(self, message):
+        #     self.CreateInitButtons(message)
+
+        # # first level callbacks
+        # @self.bot.message_handler(func=lambda message: message.text == "Map")
+        # def characters_reply(self, message):
+        #     self.CreateNpcButtons(message)
+
+        # @self.bot.message_handler(func=lambda message: message.text == "Items")
+        # def items_reply(self, message):
+        #     self.CreateItemsButtons(message)
+
+        # # second level callbacks
+        # @self.bot.message_handler(func=lambda message: message.text == "Move 1")
+        # def handle_cButton1(self, message):
+        #     self.send_message(message.chat.id, "cButton 1 pressed")
+
+
+        # @self.bot.message_handler(func=lambda message: message.text == "Move 2")
+        # def handle_cButton2(self, message):
+        #     self.send_message(message.chat.id, "cButton 2 pressed")
+
+        # @self.bot.message_handler(func=lambda message: message.text == "cBack")
+        # def handle_cBack(self, message):
+        #     self.CreateInitButtons(message)
+
+        # @self.bot.message_handler(func=lambda message: message.text == "iButton 1")
+        # def handle_iButton1(self, message):
+        #     self.send_message(message.chat.id, "iButton 1 pressed")
+
+        # @self.bot.message_handler(func=lambda message: message.text == "iButton 2")
+        # def handle_iButton2(self, message):
+        #     self.send_message(message.chat.id, "iButton 2 pressed")
+
+        # @self.bot.message_handler(func=lambda message: message.text == "iBack")
+        # def handle_iBack(self, message):
+        #     self.CreateInitButtons(message)
+
+#functions
+#********************************************************************************
--- world_map_class.py
+++ world_map_class.py
@@ -1,52 +1,8 @@
-from db.database import DB
-
-db = DB()
-
-class Location:
-    game_locations = db.fill_data('db/locations.json')
-    game_enemies = db.fill_data('db/enemies.json')
-    game_npcs = db.fill_data('db/npcs.json')
-
-    def __init__(self, key: str):
-        data = Location.game_locations[key]
-        self.name: str = data["name"]
-        self.description: str = data["description"]
-        self.linked_locations: list[str] = data["linked_locations"]
-        self.npc: list[str] = data["npc"]
-        self.enemies: list[str] = data["enemies"]
-        self.id = key
-
-    def get_linked_locations(self):
-        linked_locations = [Location.game_locations[location_key]
-                            for location_key in self.linked_locations]
-        return linked_locations
-
-    def get_linked_location_id_by_name(self, name):
-        for location_key in self.linked_locations:
-            if Location.game_locations[location_key]["name"] == name:
-                return location_key
-
-    def get_full_npcs_data(self):
-        npc_data = [Location.game_npcs[npc_key]
-                    for npc_key in self.npc]
-        return npc_data
-
-    def get_enemies(self):
-        linked_enemies = [{enemy_key : Location.game_enemies[enemy_key]["name"]}
-                          for enemy_key in self.enemies]
-        return linked_enemies
-
-    def get_full_enemies_data(self):
-        linked_enemies = [Location.game_enemies[enemy_key] for enemy_key in self.enemies]
-        return linked_enemies
-
-    def get_description(self):
-        return self.description
-
-    def get_npcs(self):
-        linked_enemies = [{npc_key: Location.game_npcs[npc_key]["name"]}
-                          for npc_key in self.npc]
-        return linked_enemies
+class Location():
+    def __init__(self, data: dict):
+        self.name = data.keys()[0]
+        self.description = data["description"]
+        self.linked_locations = data["linked_locations"]
 
     def __str__(self):
         return self.name
@@ -57,21 +13,16 @@ class Location:
     def __eq__(self, other):
         if isinstance(other, Location):
             return (self.name == other.name
-                    and self.linked_locations == other.linked_locations
-                    )
+                and self.linked_locations == other.linked_locations
+                )
 
     def __hash__(self):
         return hash((self.name,
                      self.description,
                      self.linked_locations
-                     ))
+                    ))
 
     def __ne__(self, other):
         if isinstance(other, Location):
             return not self.__eq__(other)
 
-
-if __name__ == "__main__":
-    loc = Location("location_1")
-    # print(len(loc.get_linked_locations()))
-    print(loc.get_npcs())
